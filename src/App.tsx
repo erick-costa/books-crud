cf18dd01b43b72f919e5f17122edef8b4b2f282f
Erick Costa do Nascimento <erickcosta2509@gmail.com>
2026-02-04 21:25:55 -0300

    updated layout style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Layout, Typography, Tabs, Card } from "antd"
 import { AuthorsPage } from "./pages/Authors"
 import { BooksPage } from "./pages/Books"
+import "./index.css"
 
 const { Header, Content } = Layout
 const { Title } = Typography
@@ -8,7 +9,14 @@ const { Title } = Typography
 export default function App() {
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Header style={{ background: "#fff", padding: "0 24px" }}>
+      <Header
+        style={{
+          background: "#fff",
+          padding: "24px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
         <Title level={3} style={{ margin: 0 }}>
           Books & Authors
         </Title>
